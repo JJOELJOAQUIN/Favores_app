8aaea22596a7717b9fafb82c9cb32762bdd1f345
JLOPEZAUDI <jlopez@audifarmsalud.com>
2025-11-05 20:53:13 -0300

    Subida inicial de la solución .NET

---
+++ Favores_Back-mvc/Context/EscuelaDBContext.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Favores_Back_mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Favores_Back_mvc.Context
+
+{
+    public class EscuelaDBContext : DbContext
+    {
+        public EscuelaDBContext(DbContextOptions<EscuelaDBContext>
+        options) : base(options)
+        {
+        }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+    }
+}
---
+++ Favores_Back-mvc/Models/Deporte.cs
@@ -0,0 +1,12 @@
+﻿namespace Favores_Back_mvc.Models
+{
+    public enum Deporte
+    {
+        Natacion,
+Futbol,
+Voley,
+Basquet,
+Truco,
+Ajedrez
+    }
+}
